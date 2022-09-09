b60270232dfabccbe414d99b986780620c9ad7b2
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 02:18:46 -0700

    Print without usinf printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Starting point of program
+ *Description - Program to display output using puts
+ *Return: Always 1 (Success)
+ */
+
+
+
+int main(void)
+
+
+
+{
+
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+
+
+}
