4e4646df7a5168d9f210f9264acd25f5ffa67b32
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 14:54:40 -0700

    Alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *main - Starting point of program
+ *
+ * *
+ *
+ * * Description: Program to print alphabets except q and e
+ *
+ * *
+ *
+ * * Return: Always 0 success
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	char alphabet;
+
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	{
+
+		if (alphabet != 'q' && alphabet != 'e')									putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
