5964e7908cc59e3b0461cbfd9344df9dbdb7b2bb
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 01:38:19 -0700

    Use of puts() to display output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-
-
 /**
  *
  * *main - Starting point of program
  *
- * *
+ * 
  *
- * *Description - Program to display output using puts
+ * Description - Program to display output using puts
  *
- * *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
- * */
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
