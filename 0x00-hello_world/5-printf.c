369bd671678d62a8dd5c0b16768c1e6865a36cb2
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 01:47:11 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Starting point of program
+ *Description - Program to display output using printf*
+ *Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+ 	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+
+
+}
