98d4fe9da7e1b2ada830be8ef587f73de5259c0a
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 01:40:59 -0700

    Use of puts() to display output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 /**
+ *main - Starting point of program
  *
- * *main - Starting point of program
- *
- * 
- *
- * Description - Program to display output using puts
- *
- * Return: Always 0 (Success)
- *
+ *Description - Program to display output using puts
+ *Return: Always 0 (Success)
  */
 
 int main(void)
