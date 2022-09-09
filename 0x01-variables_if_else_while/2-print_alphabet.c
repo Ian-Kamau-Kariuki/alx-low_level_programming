de1396d00e0b4c6254fd749e7031e65232a3b6c6
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 14:37:06 -0700

    pritn alphavet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Starting point of program
+ *Description: Program to print alphabets in lowercase
+ *Return: Always 0 Success
+ */
+
+
+
+int main(void)
+
+{
+
+	char c;
+	c = 'a';
+
+
+
+	while (c <= 'z')
+{
+	putchar(c);
+	c++;
+
+												}
+
+	putchar('\n');
+
+	return (0);
+
+}
