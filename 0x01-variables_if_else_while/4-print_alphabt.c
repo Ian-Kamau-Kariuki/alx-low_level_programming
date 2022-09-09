68435439b68337708ef5295614358dcd508d4d03
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-09 14:58:43 -0700

    Alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,16 +14,13 @@
 
 
 int main(void)
-
 {
 
 	char alphabet;
 
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-
 	{
-
 		if (alphabet != 'q' && alphabet != 'e')									putchar(alphabet);
 	}
 
