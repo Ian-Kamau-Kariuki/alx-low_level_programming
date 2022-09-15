5499faf57876415766dfbeadd718def1688b0668
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-15 23:18:32 +0300

    Print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+*print_to_98 - Function to print natural numbers
+*@n: First parameter
+*
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		_putchar((98 / 10) + '0');
+		_putchar((98 % 10) + '0');
+	}
+	_putchar('\n');
+}
