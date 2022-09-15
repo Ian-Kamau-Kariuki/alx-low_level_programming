aa72349a2c6be2601de3c7e1442ba967d9b14485
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-15 22:58:55 +0300

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_sign - Function to determine sign of a number
+*@c: First parameter
+*
+* Return: 1 if positive, 0 if 0 else negative -1
+*
+*/
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -11,22 +11,24 @@ int main(void)
 	int r;
 	/*print_alphabet();*/
 	/*print_alphabet_x10();*/
-	/*
-	 * r = _islower('H');
-	*_putchar(r + '0');
-	*r = _islower('o');
-	*_putchar(r + '0');
-	*r = _islower(108);
-	*_putchar(r + '0');
-	*_putchar('\n');
-	*/
-	r = _isalpha('H');
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _isalpha('o');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _isalpha(108);
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _isalpha(';');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
 
