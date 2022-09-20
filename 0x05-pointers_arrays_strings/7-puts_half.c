b1dd6f611255202a7cddc1577056b631e0df0498
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-09-20 23:26:39 +0300

    prints last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - takes string as input
+ *
+ * @str: string to be taken
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = _strlen(str);
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (len - 1) / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
