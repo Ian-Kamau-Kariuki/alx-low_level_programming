4f987935e62f1bf476b7962bfd7bfeec1bf8002f
Ian-Kamau-Kariuki <iangunzkamau@gmail.com>
2022-10-06 22:40:04 +0300

    more malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,51 +2,32 @@
 #include <stdlib.h>
 
 /**
-  * string_nconcat - ...
-  * @s1: ...
-  * @s2: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ *_realloc - Reallocates memory
+ *@ptr: Pointer
+ *@old_size: Bytes to be allocated
+ *@new_size: new size in bytes
+ *
+ *Return: Void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-	while (j < l)
+	if (ptr == NULL)
 	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		ptr = malloc(new_size);
+		return (ptr);
 	}
-	str[j] = '\0';
-	return (str);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	free(ptr);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
